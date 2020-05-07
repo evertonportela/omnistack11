64c449b82bb3c22201c547c9e2c9a337ccfed06f
evertonportela2 <45774181+evertonportela2@users.noreply.github.com>
2020-05-07 09:05:52 -0300

    front-end do app

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './global.css'
+
+import Routes from './routes';
+
+function App(){
+  return(
+    <Routes />
+  );
+}
+
+export default App;
+
+
+
+
+
+
+// JSX (JavaScript XML)
+/**
+ * import React, {useState} from 'react';
+ * 
+ * function App() {
+  // useState retorna um array definido abaixo:
+  // Array [valor, funcaoDeAtualizaçãoDoValor]
+  const [counter, setCounter] = useState(0);
+
+  function increment(){
+    setCounter(counter + 1);
+
+    console.log(counter);
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello World
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Header title="Semana Onmistack">
+        Contador: {counter}
+      </Header>
+      <button onClick={increment}>Incrementar</button>
     </div>
   );
 }
 
 export default App;
+ */
---
+++ frontend/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// export default function Header(props){
+export default function Header({ children }){
+    return (
+        <header>
+            {/* <h1>{props.title}</h1> */}
+            {/* <h1>{props.children}</h1> */}
+            <h1>{children}</h1>
+        </header>
+    )
+};
+
+//export default Header;
